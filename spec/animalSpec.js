d7d6db9e30398a9397768c85f91467f7f00a1121
Claire Nessler <clrnessler@gmail.com>
2015-07-13 11:25:50 -0600

    finished exercises

--- spec/animalSpec.js
+++ spec/animalSpec.js
@@ -10,45 +10,46 @@ describe('Animal', function(){
 
   describe('#oink', function(){
     it('should oink if it is a pig', function(){
-
+      expect(animal.oink()).toEqual('oink');
     });
     it('should not oink if it is not pig', function(){
-
+      animal.kind = 'Lion';
+      expect(animal.oink()).toEqual('I am not a pig');
     });
   });
 
   describe('#growUp', function(){
     it('should increment the age by one', function(){
-
+      expect(animal.growUp()).toEqual(28);
     });
   });
 
   describe('#awake', function(){
     it('should initially be off', function(){
-
+      expect(animal.awake).toEqual(false);
     });
   });
 
    describe('#wakeUp', function(){
     it('should change state to awake', function(){
-
+      expect(animal.wakeUp()).toEqual(true);
     });
   });
 
   describe('#sleep', function(){
     it('should initially be sleeping', function(){
-
+      expect(animal.sleep()).toEqual(false);
     });
   });
 
   describe('#feed', function(){
     it('should get fed if awake', function(){
-
+      animal.wakeUp();
+      expect(animal.feed()).toEqual('Nom Nom');
     });
     it('not should get fed if sleeping', function(){
-
+      // animal.awake = false;
+      expect(animal.feed()).toEqual('zzzz');
     });
   });
 });
-
-
--- src/animal.js
+++ src/animal.js
@@ -1,9 +1,41 @@
-function Animal(){
-
+function Animal(name, age, kind){
+  this.name = name;
+  this.kind = kind;
+  this.age = age;
+  this.awake = false;
 }
 
-Animal.prototype.oink = function(){
 
-};
+Animal.prototype.oink = function(){
+  if (this.kind === 'Pig'){
+  return 'oink';
+  } else {
+  return 'I am not a pig';
+  }
+}
+
+Animal.prototype.growUp = function () {
+  return this.age + 1;
+}
+
+Animal.prototype.awake = function () {
+  return this.awake;
+}
+
+Animal.prototype.wakeUp = function () {
+  return this.awake = true;
+}
+
+Animal.prototype.sleep = function () {
+  return this.awake;
+}
+
+Animal.prototype.feed = function () {
+  if (this.awake === true){
+    return 'Nom Nom';
+  } else{
+    return 'zzzz';
+  }
+}
 
 module.exports=Animal;
